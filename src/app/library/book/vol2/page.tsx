165ece0c6b92e4732b140efe7e8a0b906732ed68
Harry-DWAC <harix64@icloud.com>
2026-05-29 08:25:58 +0800

    fix: update page.tsx title and SEO

--- src/app/library/book/vol2/page.tsx
+++ src/app/library/book/vol2/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'DWAC Professional Library (Volume 2) - DWAC',
+  title: 'DWAC Professional Library (Volume 2) ',
   description: 'Advanced practices and case studies in digital world dispute resolution.',
 }
 
